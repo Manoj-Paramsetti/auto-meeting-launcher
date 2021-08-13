6988ee43bac599d16005f9d2cd97dd07685ad567
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-08-13 16:05:02 +0530

    Add: launcher for google meet

--- main.py
+++ main.py
@@ -7,46 +7,14 @@ import yaml
 import pyautogui
 import tkinter
 
-file=__file__[:-7]
+file = __file__[:-7]
 
 meetings = {"meetings": []}
 
 
-def open_zoom_link(link):
-    webbrowser.open(link)
-    f.close()
-    print(" Waiting for browser to open", end="")
-    launch_zoom("/")
-
-
-def open_zoom(_alias):
-    key = list(_alias)[0]
-    print()
-    link = _alias[key]
-    webbrowser.open(link)
-    print(" Waiting for browser to open", end="")
-    launch_zoom("/")
-
-
-def launch_zoom(loading_symbol):
-    try:
-        x, y = pyautogui.locateCenterOnScreen(
-            file+"Zoom-Launch-Meeting-Button.png", confidence=0.8
-        )
-        pyautogui.click(x, y)
-
-    except TypeError:
-        if loading_symbol == "\\":
-            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
-            launch_zoom("/")
-        else:
-            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
-            launch_zoom("\\")
-
-
 def more_option(opt):
     os.system("cls || clear")
-
+    
     # Add
     if opt == 1:
         print(" Give alias for your meeting")
@@ -56,12 +24,14 @@ def more_option(opt):
         link = input(" [User] >>> ")
 
         meetings["meetings"].append(dict({alias: link}))
-
-        with open(file+"zoom.yml", "w") as f:
-            yaml.dump(meetings, f)
-        print(" Data is added")
-        input("\n [ENTER] >>> ")
-        print()
+        if validate(link):
+            with open(file + "zoom.yml", "w") as f:
+                yaml.dump(meetings, f)
+            print(" Data is added")
+            input("\n [ENTER] >>> ")
+            print()
+        else:
+            more_option(1)
 
     # Remove
     elif opt == 2:
@@ -74,7 +44,7 @@ def more_option(opt):
         print(" Type the id number to delete")
         alias = int(input(" [User] >>> "))
         meetings["meetings"].pop(alias - 1)
-        with open(file+"zoom.yml", "w") as f:
+        with open(file + "zoom.yml", "w") as f:
             yaml.dump(meetings, f)
         print(" removed")
         input("\n [ENTER] >>> ")
@@ -102,9 +72,9 @@ def more_option(opt):
         print(" Select the meeting number to start")
         alias = int(input(" [User] >>> "))
         print()
-        open_zoom(meetings["meetings"][alias - 1])
+        open_key_link(meetings["meetings"][alias - 1])
 
-    # help
+    # Help
     elif opt == 5:
         display_help()
         input("\n [ENTER] >>> ")
@@ -118,6 +88,7 @@ def more_option(opt):
     start()
 
 
+# Displaying Help
 def display_help():
     print("\n =====================================================")
     print(" To open your meeting from command line")
@@ -142,7 +113,14 @@ def display_help():
     print(" =====================================================\n")
 
 
+# menu
 def start():
+    os.system("cls || clear")
+    print(""" __  __     __      __    __  __  _  _  ___  _   _  ____  ____
+(  \\/  )___(  )    /__\\  (  )(  )( \\( )/ __)( )_( )( ___)(  _ \\
+ )    ((___))(__  /(__)\\  )(__)(  )  (( (__  ) _ (  )__)  )   /
+(_/\\/\\_)   (____)(__)(__)(______)(_)\\_)\\___)(_) (_)(____)(_)\\_)
+""")
     print("==================")
     print("1. Add meeting")
     print("2. Remove meeting")
@@ -159,27 +137,134 @@ def start():
         start
     more_option(opt)
 
-
+# GUI
 def gui():
-    window_main = tkinter.Tk(className="Zoom Auto Launcher")
-    tkinter.Label(text="Select your meeting to open").pack()
+    window_main = tkinter.Tk(className=" Meeting Launcher")
+    frame = tkinter.Frame(window_main)
+    frame.pack(expand=True, padx=10, pady=20)
+    tkinter.Label(frame, text="Select your meeting to open", font=("Arial", 12)).pack(
+        pady=10
+    )
     for meeting in meetings["meetings"]:
         x = meeting.keys()
         tkinter.Button(
-            window_main,
+            frame,
             text=list(x)[0],
             height="2",
-            width="80",
-            command=lambda link=meeting[list(x)[0]]: open_zoom_link(link),
-        ).pack()
+            width="40",
+            bg="white",
+            command=lambda link=meeting[list(x)[0]]: find_whom_to_call(link),
+        ).pack(padx=10)
     window_main.mainloop()
+
+
+# link opener
+def open_link(link):
+    webbrowser.open(link)
+    f.close()
+    print(" Waiting for browser to open", end="")
+
+# banana peeler :) (gives link from yaml)
+def open_key_link(_alias):
+    key = list(_alias)[0]
+    print()
+    link = _alias[key]
+    find_whom_to_call(link)
+
+
+# LAUNCHER
+
+
+def launch_zoom(loading_symbol):
+    try:
+        x, y = pyautogui.locateCenterOnScreen(
+            file + "Zoom-Launch-Meeting-Button.png", confidence=0.8
+        )
+        pyautogui.click(x, y)
+
+    except TypeError:
+        if loading_symbol == "\\":
+            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
+            launch_zoom("/")
+        else:
+            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
+            launch_zoom("\\")
+
+
+def launch_gmeet(loading_symbol):
+    try:
+        x, y = pyautogui.locateCenterOnScreen(
+            file + "google_meet_join.png", confidence=0.8
+        )
+        print(x, y)
+        pyautogui.click(x, y)
+
+    except TypeError:
+        if loading_symbol == "\\":
+            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
+            launch_gmeet("/")
+        else:
+            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
+            launch_gmeet("\\")
+
+
+# def launch_teams(loading_symbol):
+#    try:
+#        x, y = pyautogui.locateCenterOnScreen(
+#            file+"Zoom-Launch-Meeting-Button.png", confidence=0.8
+#        )
+#        pyautogui.click(x, y)
+#
+#    except TypeError:
+#        if loading_symbol == "\\":
+#            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
+#            launch_teams("/")
+#        else:
+#            print("\r Waiting for browser to open   [" + loading_symbol + "]", end="")
+#            launch_teams("\\")
+
+
+def find_whom_to_call(link):
+    if re.search("meet.google.com", link):
+        open_link(link)
+        launch_gmeet("\\")
+    elif re.search("zoom", link):
+        open_link(link)
+        launch_zoom("\\")
+    elif re.search("teams.live", link):
+        print(" Chori....")
+        print(" I'm can't launch microsoft teams but I can open it for you")
+        input(" [ENTER] >>> ")
+        open_link(link)
+        # launch_teams("\\")
+    else:
+        print("Wrong link")
+        start()
+
+
+def validate(link):
+    if re.search("meet.google.com", link):
+        return True
+    elif re.search("zoom", link):
+        return True
+    elif re.search("teams.live", link):
+        print(" Chori....")
+        print(" I'm can't launch microsoft teams but I can open it for you")
+        input(" [ENTER] >>> ")
+        return True
+        # launch_teams("\\")
+    else:
+        print(" Wrong link")
+        print(" Try again")
+        input(" [ENTER] >>> ")
+        return False
 
 
 try:
     if __name__ == "__main__":
 
         try:
-            with open(file+"zoom.yml", "r") as f:
+            with open(file + "zoom.yml", "r") as f:
                 meetings = yaml.load(f, Loader=yaml.FullLoader)
         except FileNotFoundError:
             print("Give alias for your meeting")
@@ -190,9 +275,10 @@ try:
 
             meetings["meetings"].append(dict({alias: link}))
 
-            with open(file+"zoom.yml", "w") as f:
+            with open(file + "zoom.yml", "w") as f:
                 yaml.dump(meetings, f)
-            open_zoom_link(link)
+            # open_link(link)
+            find_whom_to_call(link)
 
         if len(sys.argv) == 1:
             start()
@@ -202,7 +288,7 @@ try:
                 i = int(arg1)
                 print()
                 try:
-                    open_zoom(meetings["meetings"][i - 1])
+                    open_key_link(meetings["meetings"][i - 1])
                 except IndexError:
                     print("There is no data on that number")
                     print("Press ENTER to get the menu. To exit press CTRL+C")
@@ -216,4 +302,10 @@ try:
                 display_help()
 
 except KeyboardInterrupt:
+    os.system("cls || clear")
+    print(""" __  __     __      __    __  __  _  _  ___  _   _  ____  ____
+(  \\/  )___(  )    /__\\  (  )(  )( \\( )/ __)( )_( )( ___)(  _ \\
+ )    ((___))(__  /(__)\\  )(__)(  )  (( (__  ) _ (  )__)  )   /
+(_/\\/\\_)   (____)(__)(__)(______)(_)\\_)\\___)(_) (_)(____)(_)\\_)
+""")
     print("\n\rExited Successfully")
